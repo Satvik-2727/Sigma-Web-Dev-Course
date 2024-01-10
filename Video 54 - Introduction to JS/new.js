e2e87b792f780c12858510c16f6d6fd0b46872b3
Satvik-2727 <techblogs2021@gmail.com>
2024-01-10 18:56:16 +0530

    Completed Vid 54

---
+++ Video 54 - Introduction to JS/new.js
@@ -0,0 +1,4 @@
+console.log("Hello world!");
+console.log("The Code is Running...")
+console.log("The Code is also Running...")
+console.log("The Code is Running like a wow!...")
---
+++ Video 54 - Introduction to JS/script.js
@@ -0,0 +1,20 @@
+alert("Hello World")
+
+console.log("The Code is Running...")
+console.log("The Code is also Running...")
+console.log("The Code is Running like a wow!...")
+
+var a = prompt("Enter your number")
+console.log("Your number is " + a)
+
+var isTrue = confirm("Are you sure you want to continue")
+
+if (isTrue) {
+    console.log("You have continued");
+}
+
+else{
+    console.log("You have not continued");
+}
+
+document.title = "I'am there , I listened"
