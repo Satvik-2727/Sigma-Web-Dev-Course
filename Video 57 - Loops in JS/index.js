6dd3305f0911432bb4bb99a2708dbb3580dabda4
Satvik-2727 <techblogs2021@gmail.com>
2024-01-10 19:31:33 +0530

    Completed Vid 57

---
+++ Video 57 - Loops in JS/index.js
@@ -0,0 +1,32 @@
+console.log("Tutorial on Loops");
+let a = 1
+// for(let i = 0; i<100; i++){
+//     console.log(a+i);
+// }
+
+let obj = {
+    name:"Satvik",
+    role:"Programmer",
+    comapny:"RAIN"
+}
+
+// for (const key in obj) {
+//     const element = obj[key];
+//     console.log(key)
+// }
+
+// for (const c of "Coder") {
+//     console.log(c)
+// }
+
+let i = 20;
+while(i<6){
+    console.log(i);
+    i++;
+}
+
+do {
+    console.log(i);
+    i++;
+    
+} while (i<6);
