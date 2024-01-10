56e3ce88c292735ca4908630c32e4fa99240ee80
Satvik-2727 <techblogs2021@gmail.com>
2024-01-10 19:24:24 +0530

    Completed Vid 56

---
+++ Video 56 - If, else, if-else ladder/script.js
@@ -0,0 +1,13 @@
+console.log("Hello world!");
+
+let age = 17;
+let grace = 2;
+if((age+grace)>18){
+    console.log("You can drive");
+}
+
+else{
+    console.log("You cannot drive");
+}
+
+console.log(age+grace)
