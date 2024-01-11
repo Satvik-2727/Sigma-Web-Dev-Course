ea9bd69bb9086a1dc9c5abecabcd14b6c574382d
Satvik-2727 <techblogs2021@gmail.com>
2024-01-11 16:53:11 +0530

    Completed Vid 60

---
+++ Video-60 - Strings in JS/index.js
@@ -0,0 +1,21 @@
+console.log("This is strings tutorial");
+
+let a = "hello"
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+
+
+console.log(a.length);
+
+let name = "Satvik"
+let friend = "Sathvika"
+
+console.log("His name is " + name + " and his friend is " + friend);1
+console.log(`His name is ${name} and his friend is "${friend}`);
+
+
+console.log(friend.concat("Aishwarya"))
+console.log()
